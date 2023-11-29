0cd71013ea8e918a902664e43068e4166be54cac
UpcProgramingII <142351191+UpcProgramingII@users.noreply.github.com>
2023-11-28 22:50:03 -0500

    Add files via upload

---
+++ demo-gca/src/test/java/com/example/demogca/DemoGcaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demogca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGcaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
